7bd235bc1638aca3cfad45237e75293806f3baf5
Waynewcwu <Wayne_wc_wu@umc.com>
2020-08-04 11:22:31 +0800

    FTPENS v1.0

---
+++ GPIO.py
@@ -0,0 +1,37 @@
+import time
+import RPi.GPIO as GPIO
+import logging
+from logging.config import fileConfig
+from logging.handlers import TimedRotatingFileHandler
+import datetime
+
+log_filename = datetime.datetime.now().strftime("./Log/%Y-%m-%d_%H_%M.log")
+fileConfig('./Log/logging_config.ini', defaults={"log_filename":log_filename})
+logger = logging.getLogger()
+
+BUTTON_PIN = 18
+LED_PIN = 23
+
+def GPIO_detect_callback(channel):
+	#detected massage
+	logging.info('Pressed the Button')
+	#Action after detected
+	GPIO.output(LED_PIN, GPIO.HIGH)
+	time.sleep(0.1)
+	GPIO.output(LED_PIN, GPIO.LOW)
+
+GPIO.setmode(GPIO.BCM)
+
+
+GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.add_event_detect(BUTTON_PIN, GPIO.RISING, callback=GPIO_detect_callback, bouncetime=250)
+
+try:
+	logging.info('Press Ctrl-C to Stop')
+	while True:
+		time.sleep(1)
+except KeyboardInterrupt:
+	logging.info('Shutdown Program')
+finally:
+	GPIO.cleanup()
